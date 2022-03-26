6f7ea15b0961d81a8f8f5ed1784c3cff3fa5781c
JAMESLINKCODES <81818080+JAMESLINKCODES@users.noreply.github.com>
2022-03-26 14:07:18 +0300

    Add Monte Carlo, Gauss methods

--- mathutils/integration.py
+++ mathutils/integration.py
@@ -1,3 +1,7 @@
+import random
+import numpy as np
+
+
 def validate_input(a, b, n):
     if n <= 0:
         raise ValueError("Enter n > 0 please\n")
@@ -10,7 +14,7 @@ def integrate_rectangles_method(a, b, n, func):
         return 0
     validate_input(a, b, n)
     h = (b - a) / n
-    integral = None
+    integral = 0
     for i in range(0, n):
         integral += func(a + i * h)
     return h * integral
@@ -47,10 +51,10 @@ def integrate_cubic_parabola_method(a, b, n, func):
     if a == b:
         return 0
     validate_input(a, b, n)
-    h = (b - a) / n
+    h = (b - a) / (3 * n)
     threes_sum = 0
     twos_sum = 0
-    for i in range(1, n):
+    for i in range(1, 3 * n):
         if i % 3 == 0:
             twos_sum += func(a + i * h)
         else:
@@ -62,11 +66,11 @@ def integrate_bull_method(a, b, n, func):
     if a == b:
         return 0
     validate_input(a, b, n)
-    h = (b - a) / n
+    h = (b - a) / (4 * n)
     thirty_two_sum = 0
     twelve_sum = 0
     fourteen_sum = 0
-    for i in range(1, n):
+    for i in range(1, 4 * n):
         if i % 2 != 0:
             thirty_two_sum += func(a + i * h)
         elif i % 2 == 0 and i % 4 != 0:
@@ -75,3 +79,30 @@ def integrate_bull_method(a, b, n, func):
             fourteen_sum += func(a + i * h)
 
     return ((2 * h) / 45) * (7 * func(a) + 32 * thirty_two_sum + 14 * fourteen_sum + 12 * twelve_sum + 7 * func(b))
+
+
+def integrate_monte_carlo(a, b, n, func):
+    if a == b:
+        return 0
+    validate_input(a, b, n)
+    u = np.random.rand(n, 1)
+    y = func(a + (b - a) * u)
+    return ((b - a) / n) * y.sum()
+
+
+def integrate_gauss(a, b, n, func):
+    if a == b:
+        return 0
+    validate_input(a, b, n)
+    xi_arr = [[-0.577350269, 0.577350269], [-0.774596669, 0, 0.774596669],
+              [-0.861136312, -0.339981044, 0.339981044, 0.861136312],
+              [-0.906179846, -0.538469310, 0, 0.538469310, 0.906179846],
+              [-0.932469514, -0.661209386, -0.238619186, 0.238619186, 0.661209386, 0.932469514]]
+    c_arr = [[1, 1], [0.555555556, 0.888888889, 0.555555556],
+             [0.347854845, 0.652145155, 0.652145155, 0.347854845],
+             [0.236926885, 0.478628670, 0.568888889, 0.478628670, 0.236926885],
+             [0.171324492, 0.360761573, 0.467913935, 0.467913935, 0.360761573, 0.171324492]]
+    integral = 0
+    for xi, c in zip(xi_arr[n - 2], c_arr[n - 2]):
+        integral += c * func(((a + b) + xi * (b - a)) / 2)
+    return (b - a) / 2 * integral
